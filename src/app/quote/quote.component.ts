ac97eaf227e177492833747046d0c0629b67272d
lwairore <kwairore@gmail.com>
2019-04-28 18:25:22 +0300

    Defined incrementUpVote function that increases the vote by only one and decreases the value if the upVote icon is clicked twice

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -16,7 +16,32 @@ export class QuoteComponent implements OnInit {
   completeQuote(isComplete,index){
     if(isComplete){
       this.quotes.splice(index,1)
+      
     }
+  }
+
+  
+  counter = 0;
+  totalUpVote = 78;
+  totalDownVote = 12;
+  incrementUpVote(j, index){
+    
+    
+    if(this.quotes[index].userUpVote == true){
+      this.counter = this.counter + 1;      
+      this.totalUpVote += this.counter;      
+      this.counter = 0;      
+      this.quotes[index].userUpVote = false;      
+    }
+    else if(this.quotes[index].userUpVote == false) {
+      this.totalUpVote = this.totalUpVote - 1;      
+      this.quotes[index].userUpVote = true;      
+      if(this.totalUpVote < 0) {
+        this.totalUpVote = 0;
+      }
+      
+    }
+    
   }
   constructor() { }
 
