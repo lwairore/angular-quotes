0dbf0a051baf388f2897eae6eb07b53e0b02f146
lwairore <kwairore@gmail.com>
2019-04-28 19:25:37 +0300

    Add postedOn property to Quote class with a value of Date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     public userUpVote:boolean;
     public userDownVote: boolean;
-    constructor(public wiseSaying:string, public author:string, public postedBy:string, /*public postedOn:Date,*/ ){
+    constructor(public wiseSaying:string, public author:string, public postedBy:string, public postedOn:Date, ){
         this.userUpVote = true;
         this.userDownVote = true;
     }
