75df6e6b9457083091e07f8b4cbfac0bedf86f85
lwairore <kwairore@gmail.com>
2019-04-28 11:54:18 +0300

    Create quotes variable that holds all Quote instances

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes = [
+    new Quote( 'Our greatest glory is not in never falling, but in rising every time we fall.', 'Confucius', '~john' ),
+    new Quote( 'All our dreams can come true, if we have the courage to pursue them.', 'Walt Disney', 'J.Kelly',  )
+  ]
 
   constructor() { }
 
