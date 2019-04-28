74c101849a254e0d513955194f260e4da5648d4f
lwairore <kwairore@gmail.com>
2019-04-28 11:45:05 +0300

    Create Quote class that takes in three arguments; wiseSaying, author, postedBy, and postedAt

---
+++ src/app/quote.ts
@@ -0,0 +1,8 @@
+export class Quote {
+    public userUpVote:boolean;
+    public userDownVote: boolean;
+    constructor(public wiseSaying:string, public author:string, public postedBy:string, public postedOn:Date, ){
+        this.userUpVote = true;
+        this.userDownVote = true;
+    }
+}
