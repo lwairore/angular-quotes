06df3b7685e82352c3f49196c8080a56ba5b2e65
lwairore <kwairore@gmail.com>
2019-04-28 18:53:08 +0300

    Emit isDownVote to parent, quoteComponent.

--- src/app/down-vote/down-vote.component.ts
+++ src/app/down-vote/down-vote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -8,8 +8,12 @@ import { Quote } from '../quote';
 })
 export class DownVoteComponent implements OnInit {
   @Input() quote:Quote;
+  @Output() isDownVote = new EventEmitter();
 
-  
+  downVote() {
+    this.isDownVote.emit();
+  }
+
 
 
   constructor() { }
