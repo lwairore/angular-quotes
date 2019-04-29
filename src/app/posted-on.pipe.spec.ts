03580ebcf82de24632056054f9b8b0f8538e2052
lwairore <kwairore@gmail.com>
2019-04-29 10:51:32 +0300

    Auto-generated commit

--- src/app/posted-on.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PostedOnPipe } from './posted-on.pipe';
-
-describe('PostedOnPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PostedOnPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
